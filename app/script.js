3320d86805f77f4336ca5a89f25cea51bcf98d07
Claudia Cuevas <omordn@gmail.com>
2015-03-09 20:42:44 -0700

    final, working app from lecture

--- app/script.js
+++ app/script.js
@@ -17,7 +17,21 @@ $(function() {
 			/*the respond call back is part of the res.send(quotes[randomIndex]);
 			line in the server.js*/
 			$('#ajax-text').text(resText);
-		});
-	});
+		}); // end GET method
+	}); // end button on click event
 
-});
+	$('#piglatin').on('submit', function(e) {
+		e.preventDefault();
+
+		var firstname = $('input[name=firstname]').val();
+		var lastname = $('input[name=lastname]').val();
+		var name = { firstname: firstname, lastname: lastname };
+		console.log(name);
+
+		$.post('piglatin', name, function(response) {
+			var piglatinfied = response.firstname + " " + response.lastname;
+			$('#piglatinified').text(piglatinfied);
+		}); //end post method
+	}); // end piglatin form submit event
+
+}); // end on ready
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require("express");
+var express = require("express"); // expressJS is required
 var app = express();
+var bodyparser = require("body-parser"); //body-parser is required
+var piglatinify = require("./lib/piglatinify.js");
 var port = process.env.PORT || 3000;
 
 var quotes = [
@@ -13,6 +15,9 @@ var jokes = [
 	{setup: "What do you get when you cross a cow and a duck?", punchline: "Milk and quackers."},
 	{setup: "How many tickles does it take to make an octopus laugh?", punchline: "Ten Tickles"}
 ];
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true})); //DO NOT REMOVE THIS LINE
 
 //tells express where app files are and to use them
 app.use(express.static(__dirname + "/app/"));
@@ -29,6 +34,13 @@ app.get("/quotes", function(req, res) {
 app.get("/jokes", function(req, res) {
 	var randomIndex = Math.floor(Math.random() * jokes.length);
 	res.json(jokes[randomIndex]);
+});
+
+app.post("/piglatin", function(req, res) {
+	var firstname = piglatinify(req.body.firstname);
+	var lastname = piglatinify(req.body.lastname);
+	var piglatined = { firstname: firstname, lastname: lastname };
+	res.json(piglatined);
 });
 
 app.listen(port, function() {
