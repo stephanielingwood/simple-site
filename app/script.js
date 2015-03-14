2ff4c2dcf58adfb827d20539445ac041af223919
KMSkelton <kmskelton@outlook.com>
2015-03-14 13:01:44 -0700

    morse code is working, added bars to html

--- app/script.js
+++ app/script.js
@@ -1,11 +1,11 @@
-$(function(){
-	$("button").click(function(){
-		var url = $(this).attr("id");
+$(function(){ //start a jQuery function
+	$("button").click(function(){ //this is a button click function
+		var url = $(this).attr("id"); //set the variable url to whatever is at the button's id
 
-		$.get(url, function (response) {
-			var resText;
+		$.get(url, function (response) { //get url from above, this generates a response 
+			var resText;  //declare a variable called resText
 
-			switch (url) {
+			switch (url) {  //this is a switch that tests for where the button above is looking. This is easier than a long if/else loop
 				case "quote":
 				resText = response;
 				break;
@@ -23,28 +23,31 @@ $(function(){
 				break;
 			}
 
- 		$("#ajax-text").text(resText);
+ 		$("#ajax-text").text(resText); //this calls back to the location "ajax-text" and get the text in resText
 	 	});
 	});
 
-		$("#piglatin").on("submit", function(e) {
-		 e.preventDefault();
-		var firstname = $('input[name=firstname]').val();
-		var lastname = $('input[name=lastname]').val();
-		var name = {firstname: firstname, lastname: lastname};
-			$.post("piglatin", name, function(response) {
+		$("#piglatin").on("submit", function(e) { //goes to the location piglatin. when the 'submit' button is pressed it does the function e
+		 e.preventDefault(); //e prevents the page from refreshing. Thanks jQuery
+		var firstname = $('input[name=firstname]').val(); //creates a variable called firstname and extracts the value from the location
+														  // of the "firstname" input field
+		var lastname = $('input[name=lastname]').val();   // same as above
+		var name = {firstname: firstname, lastname: lastname}; // creates a variable 'name' and creates a json object using the firstname and lastname variables
+			$.post("piglatin", name, function(response) { //sends info to the server about the 'piglatin' form, and passes 'name' through fucntion which does the 'response' action
 		 var piglatinified = response.firstname + " " +
-		 response.lastname;
-		 $("#piglatinified").text(piglatinified);
+		 response.lastname;  //the variable piglatinified is the responses for both firstname and lastname with a space between
+		 $("#piglatinified").text(piglatinified); //goes to the object location 'piglatinified' and gets the text from the variable above
 		 });
 		});
 		
-$("#morseCode").on("submit", function(e) {
+		$("#morseCode").on("submit", function(e) {
 		 e.preventDefault();
-	var morseCodeOutput= $('input[convertify]').val();
-		$.post("morseCode", morseCodeOutput, function(response){
-			var morseCodified = response.convertify;
-			var changed = $(#morseCodified).text(morseCodified);
+		var mCO = {textToConvert: $('input[name=convertify]').val()}
+		console.log(mCO);
+		$.post("morseCode", mCO, function(response){
+			var morseCodified = response.converted;
+			console.log(morseCodified);
+		 $("#morseCodified").text(morseCodified);
 		});
 	});
 });
--- server.js
+++ server.js
@@ -14,50 +14,39 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/app/"));
 
 app.get("/", function(req, res){
-	//res.send("hello, world!");
 	res.sendFile(index.html)
 });
 
 app.post("/piglatin", function(req, res) {
-	// console.log(req);
-	// console.log(req.body);
- var firstname = piglatinify(req.body.firstname);
- var lastname = piglatinify(req.body.lastname);
- var piglatined = {firstname: firstname, lastname: lastname};
+ var plFirstname = piglatinify(req.body.firstname);
+ var plLastname = piglatinify(req.body.lastname);
+ var piglatined = {firstname: plFirstname, lastname: plLastname};
  res.json(piglatined);
 });
 
 app.post("/morseCode", function (req, res) {
-	var mc = morseCode(req.body.convertify);
-console.log(mc);
-})
+console.log("server.js says: ");
+console.log(req.body);
+var mc = morseCodify(req.body.textToConvert);
+var morseConverted = {converted: mc};
+res.json(morseConverted);
+});
 
 app.get("/quote", function (req, res){
- // var randomIndex = Math.floor(Math.random()*quotes.length);
  var quoteSend = quoteLib();
  res.send(quoteSend);
 });
 
-
 app.get("/joke", function(req, res) {
- // var randomIndex = Math.floor(Math.random()*jokes.length);
  var jokeGen = jokeLib();
  res.json(jokeGen);
 });
 
 app.get("/battingAvg", function(req, res) {
- // var randomIndex = Math.floor(Math.random()*jokes.length);
  var avgGen = batAvgLib();
  res.json(avgGen);
 });
-
-
  
-// </script>
-// app.post("/rotate", function (req, res) {
-
-// }
-
 app.listen(port, function(){
 	console.log("server started on port " + port);
 });
