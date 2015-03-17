fb02ebf69465a262e254d745696b8a1c21f07e56
Stephanie Vasquez-Soltero <StephieVictoria@gmail.com>
2015-03-17 14:29:34 -0700

    Final clean up and stuffs.

--- app/script.js
+++ app/script.js
@@ -1,70 +1,52 @@
-// This script gives it functionality, it's what is interacting
-// with the user. It will send requests for data
-// from our server file.
-
-// Now when buttons are clicked When you click the button, Ajax will make an http request
-//to your server; the response will be inserted in that <p>
-//tag.
 
 $(function() {
-$( "button" ).click(function() {
-  // Looks for the button tag and on click the function
-  // will be executed
-  var url = $(this).attr("id");
-  // This assigns to url the ID of the button that was clicked
- // $(this).css("color", "blue");
-   $.get(url, function(response) {
-    // This then sends an AJAX get request in order to
-    // acquire the wanted item.
-    var resText;
 
-    if (url === "jokes") {
-      resText = response.setup + ": " + response.punchline;
+  $( "button" ).click(function() {
+    var url = $(this).attr("id");
+    $.get(url, function(response) {
 
-} else if (url === "translator") {
-  resText = response.English + " is " + response.Spanish + " in spanish.";
+      var resText;
+      if (url === "jokes") {
+        resText = response.setup + ": " + response.punchline;
+      } else if (url === "translator") {
+        resText = response.English + " is " + response.Spanish + " in spanish.";
+      } else {
+        resText = response;
+      }
 
-    }else {
-      resText = response;
+      $("#ajax-text").text(resText);
+   });
+});
+
+  $("#piglatin").on("submit", function(e) {
+    e.preventDefault();
+
+    if ($("input[name=firstname]").val() !== "" && $("input[name=lastname]").val() !== "") {
+
+      var firstname = $("input[name=firstname]").val();
+      var lastname = $("input[name=lastname]").val();
+      var name = { firstname: firstname, lastname: lastname };
+
+      $.post("piglatin", name, function(response) {
+        var piglatinified = response.firstname + " " + response.lastname;
+        $("#piglatinified").text(piglatinified);
+      });
+
+    } else {
+
+      $("#piglatinified").text("Enter both a last and first name, please.");
     }
- $("#ajax-text").text(resText);
- // This places the response text into the p tag
- // in the html file that has the ajax-text ID
-//   var newWord = array[Math.floor(Math.random() * array.length)];
-});
-});
-
-// Even handler that fires when the form is submitted
-$("#piglatin").on("submit", function(e) {
-  //The e.preventDefault() keeps the page from refreshing when the
-  //form is submitted.
- e.preventDefault();
- if ($("input[name=firstname]").val() !== "" && $("input[name=lastname]").val() !== "") {
-
- var firstname = $("input[name=firstname]").val();
- var lastname = $("input[name=lastname]").val();
- var name = { firstname: firstname, lastname: lastname };
- console.log(name)
- // Submit the obtained data via a post request
-$.post("piglatin", name, function(response) {
- var piglatinified = response.firstname + " " +
- response.lastname;
- $("#piglatinified").text(piglatinified);
-});
-} else {
-   $("#piglatinified").text("Enter both a last and first name, please.");
-}
-
-});
-
-$("#devowel").on("submit", function(e) {
-  e.preventDefault();
-  var random = $("input[name=randomword]").val();
-  var form = { Vowels: random, Count: 0 };
-  console.log(form)
-  $.post("devowel", form, function(response) {
-    var devoweled = "The word without vowels is: \"" + response.Voweless + "\" and the number of vowels removed is: " + response.Count;
-    $("#devoweled").text(devoweled);
   });
-});
+
+  $("#devowel").on("submit", function(e) {
+    e.preventDefault();
+
+    var random = $("input[name=randomword]").val();
+    var toBeDevowled = { randomWord: random };
+
+    $.post("devowel", toBeDevowled, function(response) {
+      var devoweled = "The string without vowels is: \"" + response.Voweless + "\" and the number of vowels removed is: " + response.Count;
+      $("#devoweled").text(devoweled);
+    });
+  });
 });
--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-// Require is a method that comes with node
-// The server does stuff with sent data and sends it back
-// to the script to render it to the user.
-
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-
-app.use(express.static(__dirname + "/app/"));
 var bodyparser = require("body-parser"); // Automatically parses JSON data
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-
 var piglatinify = require("./lib/piglatining.js");
 var jokes = require("./lib/jokes.js");
 var translator = require("./lib/translator.js");
 var quotes = require("./lib/quotes.js");
 var devowel = require("./lib/devowel.js");
 
+app.use(express.static(__dirname + "/app/"));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
  res.sendFile("index.html");
-});
-
-// Starts a server on local machine
-app.listen(port, function() {
-  console.log("server started on port " + port);
 });
 
 // This is a text endpoint
@@ -41,19 +31,22 @@ app.get("/translator", function(req, res) {
   res.json(translator());
 });
 
-//Post request for a form, needed route
+// Post for a form, needed route
 app.post("/piglatin", function(req, res) {
  var firstname = piglatinify(req.body.firstname); // Parser works here
  var lastname = piglatinify(req.body.lastname);
  var piglatined = { firstname: firstname, lastname:
  lastname };
  res.json(piglatined);
-
-
 });
 
-// Poste for devowel form
+// Post for devowel form
 app.post("/devowel", function(req, res) {
-  var devoweled = devowel(req.body.Vowels);
+  var devoweled = devowel(req.body.randomWord);
   res.json(devoweled);
+});
+
+// Starts a server on local machine
+app.listen(port, function() {
+  console.log("server started on port " + port);
 });
